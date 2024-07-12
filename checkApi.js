6ff4d6a948820be3dc5c7a25626bf5a02370b05d
Harsh <harsh@gmail.com>
2024-07-12 18:48:21 +0530

    ednjlwndjwkf

--- checkApi.js
+++ checkApi.js
@@ -2,9 +2,10 @@ const { default: axios } = require("axios");
 
 const checkApi = async (req, resp) => {
   try {
-    const { data } = await axios.get(
-      "http://142.93.36.1/api/v2" + req.body.path
-    );
+    let { data } = await axios.get("http://142.93.36.1/api/v2" + req.body.path);
+
+    data = typeof data[0] === "string" ? JSON.parse(data[0]) : data;
+
     resp.send(data);
   } catch (err) {
     console.log("🚀 ~ file: checkApi.js:5 ~ checkApi ~ err:", err);
