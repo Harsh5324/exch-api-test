2c13237bcbe7d280a4ccda4dbc674d3ca0cc4f8c
Harsh <harsh@gmail.com>
2024-07-30 11:53:46 +0530

    s cs d fs

--- balance.js
+++ balance.js
@@ -1,9 +1,11 @@
+const timestamp = require("unix-timestamp");
+
 const balance = async (req, resp) => {
   try {
     resp.send({
       partnerKey:
         "lEOB6coQW4dCP15QXQwwAGYVtQCpEe09eHv36alfG4ykysWGZ1BLlBHBm9W77XmL",
-      timestamp: new Date().now(),
+      timestamp: timestamp.now(),
       userId: 1,
       balance: 5000,
       status: {
