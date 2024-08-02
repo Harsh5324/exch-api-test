11a7b8faddd8447bb39330c531e2e1813a344c07
Harsh <harsh@gmail.com>
2024-08-02 12:36:11 +0530

    akndlkwefnklewnfl

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const { app, router } = require("harsh-router");
 const checkApi = require("./checkApi");
 const casinoApi = require("./casinoApis");
 const balance = require("./balance");
+const debit = require("./debit");
+const credit = require("./credit");
 
 app.listen(82);
 
 router.post("/", checkApi);
 router.post("/2", casinoApi);
 router.post("/wco/balance", balance);
+router.post("/wco/debit", debit);
+router.post("/wco/credit", credit);
