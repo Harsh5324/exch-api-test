1be69576786a3bbdbe4674a4b8da62858cf2f030
Harsh <harsh@gmail.com>
2024-08-04 12:21:33 +0530

    kndeklwnnlknrn

--- functions/db.js
+++ functions/db.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql2");
 
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "magicwin",
+// });
+
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "Check@5324",
   database: "magicwin",
-  // socketPath: "/var/run/mysqld/mysqld.sock",
+  socketPath: "/var/run/mysqld/mysqld.sock",
 });
 
 module.exports = db;
