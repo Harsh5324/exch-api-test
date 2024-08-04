f0284f39f26c8e49cb363383673310e60567c666
Harsh <harsh@gmail.com>
2024-08-04 12:29:42 +0530

    fnkwlenflwnlnwe

--- routes/post/balance.js
+++ routes/post/balance.js
@@ -2,12 +2,20 @@ const timestamp = require("unix-timestamp");
 
 const balance = async (req, resp) => {
   try {
+    const { body } = req;
+
+    let {
+      data: [{ balance }],
+    } = await getData("balance", "users", `_id = ${body.user.id}`);
+
+    balance = parseFloat(balance);
+
     resp.send({
       partnerKey:
         "lEOB6coQW4dCP15QXQwwAGYVtQCpEe09eHv36alfG4ykysWGZ1BLlBHBm9W77XmL",
-      timestamp: Math.floor(timestamp.now()),
-      userId: "mwin2",
-      balance: 5000,
+      timestamp: Math.floor(timestamp.now()).toString(),
+      userId: body.user.id,
+      balance,
       status: {
         code: "SUCCESS",
         message: "",
